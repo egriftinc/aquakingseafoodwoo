2af14395fdeceec2633e78b051460cbba3a5ba5a
yolandajiang <33134854+yolandajiang@users.noreply.github.com>
2018-05-17 01:10:30 -0700

    swiched order of button and featured products

--- front-page.php
+++ front-page.php
@@ -47,52 +47,56 @@ get_header(); ?>
 	<span class="featured_product_front_page_title_ch">游水海鮮</span>
 	<span class="featured_product_front_page_title_en">Live Seafood</span>
 </h1>
-	<?php echo do_shortcode('[featured_product_categories cats="17" per_cat="4" columns="4"]'); ?>
 <!-- below div view more button -->
 <div class="view-more-live">
 	<span class="view-more-live-ch">更多產品</span>
 	</br>
 	<span class="view-more-live-en">View More</span>
 </div>
+	<?php echo do_shortcode('[featured_product_categories cats="17" per_cat="4" columns="4"]'); ?>
+
 
 
 <h1>
 	<span class="featured_product_front_page_title_ch">新鮮海鮮</span>
 	<span class="featured_product_front_page_title_en">Fresh Seafood</span>
 </h1>
-	<?php echo do_shortcode('[featured_product_categories cats="18" per_cat="4" columns="4"]'); ?>
 <!-- below div view more button -->
 <div class="view-more-fresh">
 	<span class="view-more-fresh-ch">更多產品</span>
 	</br>
 	<span class="view-more-fresh-en">View More</span>
 </div>
+	<?php echo do_shortcode('[featured_product_categories cats="18" per_cat="4" columns="4"]'); ?>
+
 
 
 <h1>
 	<span class="featured_product_front_page_title_ch">急凍海鮮</span>
 	<span class="featured_product_front_page_title_en">Featured Frozen Seafood</span>
 </h1>
-	<?php echo do_shortcode('[featured_product_categories cats="16" per_cat="4" columns="4"]'); ?>
 <!-- below div view more button -->
 <div class="view-more-frozen">
 	<span class="view-more-frozen-ch">更多產品</span>
 	</br>
 	<span class="view-more-frozen-en">View More</span>
 </div>
+	<?php echo do_shortcode('[featured_product_categories cats="16" per_cat="4" columns="4"]'); ?>
+
 
 
 <h1>
 	<span class="featured_product_front_page_title_ch">其他食材</span>
 	<span class="featured_product_front_page_title_en">Featured Other Seafood</span>
 </h1>
-	<?php echo do_shortcode('[featured_product_categories cats="15" per_cat="4" columns="4"]'); ?>
 <!-- below div view more button -->
 <div class="view-more-other">
 	<span class="view-more-other-ch">更多產品</span>
 	</br>
 	<span class="view-more-other-en">View More</span>
 </div>
+	<?php echo do_shortcode('[featured_product_categories cats="15" per_cat="4" columns="4"]'); ?>
+
 
 
 	<div id="primary" class="content-area">
